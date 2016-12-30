7f73db00cdee2ddd5a32a7b8ee06533e94a392ba
Arian <arian93haliti@gmail.com>
2016-12-30 18:38:08 +0100

    adding Boyer

---
+++ BoyerMoore.java
@@ -0,0 +1,45 @@
+package SubStrings;
+
+public class BoyerMoore {
+    private final int R;     // the radix
+    private int[] right;     // the bad-character skip array
+
+    private char[] pattern;  // store the pattern as a character array
+    private String pat;      // or as a string
+
+    /**
+     * Preprocesses the pattern string.
+     *
+     * @param pat the pattern string
+     */
+    public BoyerMoore(String pat) {
+        this.R = 256;
+        this.pat = pat;
+
+        // position of rightmost occurrence of c in the pattern
+        right = new int[R];
+        for (int c = 0; c < R; c++)
+            right[c] = -1;
+        for (int j = 0; j < pat.length(); j++)
+            right[pat.charAt(j)] = j;
+    }
+
+    /**
+     * Preprocesses the pattern string.
+     *
+     * @param pattern the pattern string
+     * @param R the alphabet size
+     */
+    public BoyerMoore(char[] pattern, int R) {
+        this.R = R;
+        this.pattern = new char[pattern.length];
+        for (int j = 0; j < pattern.length; j++)
+            this.pattern[j] = pattern[j];
+
+        // position of rightmost occurrence of c in the pattern
+        right = new int[R];
+        for (int c = 0; c < R; c++)
+            right[c] = -1;
+        for (int j = 0; j < pattern.length; j++)
+            right[pattern[j]] = j;
+    }
